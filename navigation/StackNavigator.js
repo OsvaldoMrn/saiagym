55a96103535255befaa4280a6b72911ca0489590
Osvaldo <odominguez13@gmail.com>
2025-04-28 21:34:50 -0600

    Se añade la pantalla de descripción de los ejercicios, con el mapeo de los datos desde el json. el apartado de instrucciones queda vacio de momento

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigator from './TabNavigator';
+import SearchScreen from '../screens/SearchScreen';
+import ExerciseDescriptionScreen from '../screens/ExerciseDescriptionScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Main" component={TabNavigator} />
-      {/* Aquí irán otras pantallas como CrearRutina, DetalleEjercicio, etc. */}
+    <Stack.Navigator>
+      {/* Pantalla principal (TabNavigator maneja las pestañas) */}
+      <Stack.Screen
+        name="Home"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+      {/* Pantalla de búsqueda */}
+      <Stack.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{ title: 'Buscar Ejercicio' }}
+      />
+      {/* Nueva pantalla de descripción */}
+      <Stack.Screen
+        name="ExerciseDescription"
+        component={ExerciseDescriptionScreen}
+        options={{ title: 'Descripción del Ejercicio' }}
+      />
     </Stack.Navigator>
   );
 }
--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
-import TrainingScreen from '../screens/TrainingScreen';
 import ProfileScreen from '../screens/ProfileScreen';
-import TrainingStack from './TrainingStack';
+import TrainingScreen from '../screens/TrainingScreen'; 
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Entrenamiento" component={TrainingStack} />
-      <Tab.Screen name="Perfil" component={ProfileScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Inicio' }} />
+      <Tab.Screen name="Training" component={TrainingScreen} options={{ title: 'Entrenamiento' }} />
+      <Tab.Screen name="Profile" component={ProfileScreen} options={{ title: 'Perfil' }} />
     </Tab.Navigator>
   );
 }
